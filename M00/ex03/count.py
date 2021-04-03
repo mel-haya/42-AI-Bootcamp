86d2086968ffa1e51e0bae13472702ed21df371e
mel-haya <yas20072003@gmail.com>
2021-04-03 21:50:14 +0100

    ex3

--- M00/ex03/count.py
+++ M00/ex03/count.py
@@ -1,20 +1,33 @@
 import string
 
-def count(*args):
+def text_analyzer(*args):
     if len(args) > 1:
         print("ERROR")
         exit()
     if len(args) == 0:
         text = input("please insert a text :\n")
-        count(text)
+        text_analyzer(text)
     elif len(args) == 1:
         upper = 0
         lower = 0
         spaces = 0
         punctuation = 0
+
         for i in args[0]:
             if i.isupper():
                 upper+=1
-            elif i.
+            elif i.islower():
+                lower+=1
+            elif i == ' ':
+                spaces+=1
+            elif i in string.punctuation:
+                punctuation += 1
+        print("The text contains " ,len(args[0])," characters:")
+        print("- ", upper," upper letters")
+        print("- ", lower," lower letters")
+        print("- ", punctuation," punctuation marks")
+        print("- ", spaces," spaces")
+
+
             
 
