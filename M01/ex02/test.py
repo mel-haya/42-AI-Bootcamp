adfa021ccfb6c4e4cf7f42d8298602051c1a9e16
Mohammed mourad El hayani <mel-haya@e2r3p6.1337.ma>
2021-04-09 16:51:47 +0100

    m01/ex02

---
+++ M01/ex02/test.py
@@ -0,0 +1,11 @@
+from vector import Vector
+
+a = Vector([6,0,8])
+b = Vector([2,1,2])
+
+# c = 2 / a
+
+#print(b.size)
+
+print(repr(a))
+
--- M01/ex02/vector.py
+++ M01/ex02/vector.py
@@ -3,7 +3,7 @@ class Vector:
         if isinstance(values,list):
             self.values = values
         elif isinstance(values,int):
-            i = 0.0
+            i = 0
             self.values = []
             while i < values:
                 self.values.append(i)
@@ -14,10 +14,125 @@ class Vector:
             raise ValueError("Invalid Vector value")
         self.size = len(self.values)
     def __add__(self,other):
-        count = max(self.size,other.size)
-        ret = Vector(count)
-        
+        ret = Vector(self.size)
+        i = 0
+        if isinstance(other, (int, float)):
+            while i < self.size:
+                ret.values[i] = self.values[i] + other
+                i+=1
+            return ret
+        elif isinstance(other, Vector):
+            if self.size != other.size:
+                raise ValueError("the two vectors have diffrent dimentions")
+            while i < self.size:
+                ret.values[i] = self.values[i] + other.values[i] 
+                i+=1
+        else:
+            raise ValueError("invalid addition")
+        return ret
+
+    
+    def __radd__(self,other):
+        ret = Vector(self.size)
+        i = 0
+        if not isinstance(other, (int, float)):
+            raise ValueError("invalid addition")
+        while i < self.size:
+            ret.values[i] = self.values[i] + other
+            i+=1
+        return ret
 
 
+    def __sub__(self,other):
+        ret = Vector(self.size)
+        i = 0
+        if isinstance(other, (int, float)):
+            while i < self.size:
+                ret.values[i] = self.values[i] - other
+                i+=1
+            return ret
+        elif isinstance(other, Vector):
+            if self.size != other.size:
+                raise ValueError("the two vectors have diffrent dimentions")
+            while i < self.size:
+                ret.values[i] = self.values[i] - other.values[i] 
+                i+=1
+        else:
+            raise ValueError("invalid subtraction")
+        return ret
+
+    def __rsub__(self,other):
+        ret = Vector(self.size)
+        i = 0
+        if not isinstance(other, (int, float)):
+            raise ValueError("invalid subtraction")
+        while i < self.size:
+            ret.values[i] = - self.values[i] + other
+            i+=1
+        return ret
+
+    def __mul__(self,other):
+        ret = 0
+        i = 0
+        if isinstance(other, (int, float)):
+            while i < self.size:
+                ret += self.values[i] * other
+                i+=1
+            return ret
+        elif isinstance(other, Vector):
+            if self.size != other.size:
+                raise ValueError("the two vectors have diffrent dimentions")
+            while i < self.size:
+                ret += self.values[i] * other.values[i] 
+                i+=1
+        else:
+            raise ValueError("invalid dot product")
+        return ret
+    
+    def __rmul__(self,other):
+        ret = 0
+        i = 0
+        if not isinstance(other, (int, float)):
+            raise ValueError("invalid dot product")
+        while i < self.size:
+            ret += self.values[i] * other
+            i+=1
+        return ret
+
+
+    def __truediv__(self, other):
+        ret = 0
+        i = 0
+        if other == 0:
+            raise ValueError("div on zero")
+        if isinstance(other, (int, float)):
+            while i < self.size:
+                ret += self.values[i] / other
+                i+=1
+            return ret
+        else:   
+            raise ValueError("invalid division")
+    
+
+    def __rtruediv__(self, other):
+        ret = 0
+        i = 0
+        if 0 in self.values:
+            raise ValueError("div on zero")
+
+        if isinstance(other, (int, float)):
+            while i < self.size:
+                ret += other / self.values[i]
+                i+=1
+            return ret
+        else:   
+            raise ValueError("invalid division")
+
+    def __str__(self):
+        return "this " + str(self.size) + "D vector values are " + str(self.values)
+
+    def __repr__(self):
+        ret = "Vector(" + str(self.size) + "," + str(self.values) + ")"
+        return ret
 
 
